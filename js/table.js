e5e0df532e98b88b6d5da324df0a2f4c5ced0b54
Jayson-Manas <jaysonmanas@gmail.com>
2018-12-14 06:23:03 +0200

    user input

--- js/table.js
+++ js/table.js
@@ -1,1 +1,34 @@
-  
+// definition
+// The prepend() method inserts specified content at the beginning of the selected elements.
+// Tip: To insert content at the end of the selected elements, use the append() method.  
+
+$(document).ready(function(){
+    // $("li").add("");
+
+    $("#btn1").click(function(){
+      $(".prep").prepend("<b>Prepended text</b>. ");
+    $("li").remove();
+
+    });
+    $("#btn2").click(function(){
+      $("ol").prepend("<li>Prepended item</li>");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#btn3").click(function(){
+      $(".prep").append(" <b>Appended text</b>.");
+    });
+    $("#btn4").click(function(){
+      $("ol").append("<li>Appended item</li>");
+    });
+  });
+
+// ===============================user input=============================
+function myFunction() {
+  var person = prompt("Please enter your name", "Harry Potter");
+  if (person != null) {
+      document.getElementById("demo").innerHTML =
+      "Hello " + person + "! How are you today?";
+  }
+}
